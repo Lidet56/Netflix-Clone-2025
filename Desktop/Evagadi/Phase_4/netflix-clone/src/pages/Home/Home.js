70907abc95b2b4e090ae22fa2199492f9511c058
Lidet56 <legesselidet@gmail.com>
2025-12-08 17:07:27 -0500

    banner

--- Desktop/Evagadi/Phase_4/netflix-clone/src/pages/Home/Home.js
+++ Desktop/Evagadi/Phase_4/netflix-clone/src/pages/Home/Home.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import Banner from '../../components/Banner/banner.js'
+
 
 const Home = () => {
   return (
     <>
         <Header/>
-     
+        <Banner/>
         <Footer/>
+
+
     </>
   )
 }
